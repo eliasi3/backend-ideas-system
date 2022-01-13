8a98b21e33e1a2c054e615a10ef5de5b922e9627
gessi <gessilane.i3solucoes@gmail.com>
2022-01-13 17:09:54 -0300

    Implemntação da adição de imagem na tabela de comentario

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :idea
   belongs_to :user
+
+  def self.upload_arquivo(arquivo, diretorio)
+        
+    FileUtils.mkdir(diretorio) unless File.exists?(diretorio)
+  
+    File.open(File.join(diretorio, arquivo.original_filename), "wb") { |f| f.write(arquivo.read) }
+  end
+  
 end
